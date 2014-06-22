6dc3e020a8a7a27cccd2f4f27f58c65c7b4281ed
Kenichi <hannousei@gmail.com>
2014-06-22 20:00:30 +0900

    Revert "tmp"
    This reverts commit 32111861a00f2f1c0ef8b5e76c1688d7825ad88e.

--- src/EqualButton.java
+++ src/EqualButton.java
@@ -34,13 +34,12 @@ public class EqualButton extends JButton implements MouseListener {
 		
 		/**後置記法に変換*/
 		textField.RPN_transform("=");
+		System.out.println(textField.RPN);
 		
-		System.out.println(Arrays.toString(textField.RPN));//確認
-		
-		/**演算を行う*/
 		textField.operation(textField.RPN, textField.flag);
 		
-		textField.setText(textField.RPN[0]);
+		
+		textField.setText(Arrays.toString(textField.RPN));
 		
 	}
     /** 今回は使用しない */
